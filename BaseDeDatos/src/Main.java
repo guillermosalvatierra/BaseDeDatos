a329e782b4a93b5eae78eae34a8a55a9ec12caec
guillermosalvatierra <guillermosalvatierra@gmail.com>
2017-06-02 21:21:02 -0300

    Se agrega el class for name

--- BaseDeDatos/src/Main.java
+++ BaseDeDatos/src/Main.java
@@ -3,60 +3,29 @@ import java.util.List;
 
 public class Main {
 
+	public int Caleb;
 	public static void main(String[] args) {
 
-	List<Object> lista = new ArrayList<Object>();
+		
+/*	List<Object> lista = new ArrayList<Object>();
 	lista.add(new C1(1,2));
 	lista.add(new C1(3,4));
 	lista.add(new C2(true));
 
 	HerramientaGenerica h =new HerramientaGenerica();
 	
-
 	for (Object c: lista){
 		h.mostrarAtributoDeClase(c);
 		h.mostrarValorAtributoDeClase(c);
 	}
-	
-	
-	
-	
-	
-/*	
-	Class clase = C1.class;
+*/	
 
-	Field[] atributos = clase.getFields();
+	ObtenedorDeInformacionPorClase o = new ObtenedorDeInformacionPorClase();
 	
-	for (Field atr: atributos){
-		Field atributo = atr;
-		String nombreAtributo = atributo.getName();
-		System.out.println("Nombre del Atributo: " + nombreAtributo);
-	}
-
-	for (C1 c: lista){
-		System.out.println( c.variableEntero);
-	}
-*/
+	o.mostrarAtributoDeClase("Main");
+	o.mostrarTipoDeAtributoDeClase("Main");
 	
 	
-	
-/*	for (Object c: lista){
-		
-		
-		Class clase = c.getClass();
-
-		Field[] atributos = clase.getFields();
-		
-		Field atributo = atributos[0];
-		String nombreAtributo = atributo.getName();
-		System.out.println("Nombre del Atributo: " + nombreAtributo);
-
-		
-		System.out.println( c);
-		
-	}
-	
-	*/
 	}	
 	
 }
---
+++ BaseDeDatos/src/ObtenedorDeInformacionPorClase.java
@@ -0,0 +1,57 @@
+import java.lang.reflect.Field;
+
+public class ObtenedorDeInformacionPorClase{
+	
+	
+	void mostrarAtributoDeClase(String nombreClase){
+		
+
+		
+		Class<? extends Object> clase;
+		try {
+			clase = Class.forName(nombreClase);
+
+			Field[] atributos = clase.getFields();
+			
+			for (Field atr: atributos){
+				Field atributo = atr;
+				String nombreAtributo = atributo.getName();
+				System.out.println("Nombre del Atributo: " + nombreAtributo);			
+			}	
+
+		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+
+	}
+	
+	void mostrarTipoDeAtributoDeClase(String nombreClase){
+		Class<? extends Object> clase;
+		try {
+			clase = Class.forName(nombreClase);
+
+			Field[] atributos = clase.getFields();
+			
+			for (Field atr: atributos){
+				Field atributo = atr;
+
+				Object tipoAtributo = atributo.getType();
+				System.out.println("Tipo del Atributo: " + tipoAtributo);		
+
+			}	
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+	
+	
+}

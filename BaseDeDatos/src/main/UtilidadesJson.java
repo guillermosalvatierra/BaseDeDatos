9f2b3929531803bd19028856c80814bc1ca0a03f
Jazsoni <JoaquinOmarJara@Gmail.com>
2017-06-09 20:00:22 -0300

    Se

--- BaseDeDatos/src/main/UtilidadesJson.java
+++ BaseDeDatos/src/main/UtilidadesJson.java
@@ -1,5 +1,20 @@
 package main;
 
 public class UtilidadesJson {
-
+	
+	public static String encapsularCadenaJson(String cadena){
+		return "{" + cadena + "}";
+	}
+	
+	public static String encapsularAtributoConSeparador(String cadena, int inicio, int fin) {
+		if (!esUltimaIteracion(inicio, fin)) {
+			cadena += ",";
+			return cadena;
+		}
+		return cadena;
+	}
+	
+	public static boolean esUltimaIteracion(int inicio, int fin) {
+		return (inicio + 1) == fin;
+	}
 }

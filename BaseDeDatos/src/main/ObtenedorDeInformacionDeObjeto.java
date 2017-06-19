8540e6eec8b6a5c67eb202d1b3468f547072b376
guillermosalvatierra <guillermosalvatierra@gmail.com>
2017-06-19 11:05:48 -0300

    cambios

--- BaseDeDatos/src/main/ObtenedorDeInformacionDeObjeto.java
+++ BaseDeDatos/src/main/ObtenedorDeInformacionDeObjeto.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Field;
 public class ObtenedorDeInformacionDeObjeto {
 
 	public Atributo traerInformacionDeAtributos(Object o){
-	
+
 		Atributo ret=traerAtributos(o,o.getClass().getSimpleName(),o.getClass().getSimpleName(),false);
 		return ret;
 	}
@@ -13,55 +13,50 @@ public class ObtenedorDeInformacionDeObjeto {
 	
 	private Atributo traerAtributos(Object o, String pnombre, String ptipo,boolean eshijo ){
 
-		Atributo atributoRet;
+		Atributo atributoC;
+		Atributo atributoR;
 		Atributo atrAux = null;
 		String tipoAtributo, nombreAtributo;
 		
 		Class<? extends Object> clase = o.getClass();
 		
 		Field[] atributos = clase.getDeclaredFields();	
-	
-
-		atributoRet = new AtributoCompuesto(pnombre,ptipo, eshijo);			
-
-
+		
+		atributoC = new AtributoCompuesto(pnombre,ptipo, eshijo);			
+		
 		for (Field atr: atributos){
 
 			nombreAtributo = atr.getName();
 			tipoAtributo   = atr.getType().getSimpleName();			
-			 atrAux = null;
+			atrAux = null;
 			atr.setAccessible(true);			
-			
-			if (atr.getType().isPrimitive()){	
+			try {			
+				if (atr.getType().isPrimitive()){	
 				
-				String sValor="";		
-				try {
+					String sValor="";		
 					sValor =atr.get(o).toString();
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-				e.printStackTrace();
-				}
-				atrAux=new AtributoSimpleObjeto(nombreAtributo,tipoAtributo,sValor);
+					atrAux=new AtributoSimpleObjeto(nombreAtributo,tipoAtributo,sValor);
 		
-			}else{
-				try {
+				}else{
 					Object valor = atr.get(o);
 					atrAux=traerAtributos(valor,nombreAtributo,tipoAtributo,true);
-
-				} catch (IllegalArgumentException | IllegalAccessException e) {
+				}			
+			} catch (IllegalArgumentException | IllegalAccessException e) {
 				e.printStackTrace();
-				}
-
-			}		
-				atributoRet.agregarHijo(atrAux);	
-		}
-	
-		if (atributos.length==1 & atrAux instanceof AtributoSimpleObjeto){
-			atributoRet=atrAux;
+			}
+			atributoC.agregarHijo(atrAux);		
 		}
 		
-		return atributoRet;
-	}
+		if (atributos.length==1 & atrAux instanceof AtributoSimpleObjeto){
+			atributoR=atrAux;
+		}else{
+			atributoR=atributoC;			
+		}
+			
+		
+		return atributoR;
 	
+}	
 	
 	
 	public String mostrarNombreDeClase(Object cx){
